2bd41c6cbe5f3e4726ff2874a9663f33dedb711e
Danzinoo <83833096+Danzinoo@users.noreply.github.com>
2021-07-07 21:33:00 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,47 @@
+# Calculator app
+
+
+
+def add(a,b):
+    result = a+b
+    print(result)
+
+def sub(a,b):
+    result=a-b
+    print(result)
+
+def mul(a,b):
+    result(a*b)
+    print(result)
+
+def div(a,b):
+    result=a/b
+    print(result)
+
+a=int(input("Enter your first number:"))
+b=int(input("Enter your second number:"))
+op=input("Enter the operator:")
+
+if op=="+":
+    add(a,b)
+
+elif op=="-":
+    sub(a,b)
+
+elif op=="*":
+    mul(a,b)
+
+elif op=="/":
+    div(a,b)
+
+else:
+    print("Invalid operator")
+
+
+
+
+
+
+
+
+
